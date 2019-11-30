2e5d8f854e99a9ed8d512b57400f464350f6e4e2
Ozgun <ozgun.abanoz@gmail.com>
2019-11-30 16:24:24 +0300

    Fixing minor issues

--- frontend/src/places/components/PlaceList.js
+++ frontend/src/places/components/PlaceList.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './PlaceList.css';
 import Card from '../../shared/components/UIElements/Card';
 import PlaceItem from './PlaceItem';
+import Button from '../../shared/components/FormElements/Button';
 
 const PlaceList = props => {
     if (props.items.length === 0) {
@@ -10,7 +11,7 @@ const PlaceList = props => {
             <div className="place-list center">
                 <Card>
                     <h2>No places found maybe create one?</h2>
-                    <button>Share place</button>
+                    <Button to="/places/new">Share place</Button>
                 </Card>
             </div>
         );
--- frontend/src/places/pages/UpdatePlace.js
+++ frontend/src/places/pages/UpdatePlace.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import './PlaceForm.css';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
+import Card from '../../shared/components/UIElements/Card';
 import {
     VALIDATOR_REQUIRE,
     VALIDATOR_MINLENGTH
@@ -52,19 +53,22 @@ const UpdatePlace = () => {
     const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
 
     useEffect(() => {
-        setFormData(
-            {
-                title: {
-                    value: identifiedPlace.title,
-                    isValid: true
+        if (identifiedPlace) {
+            setFormData(
+                {
+                    title: {
+                        value: identifiedPlace.title,
+                        isValid: true
+                    },
+                    description: {
+                        value: identifiedPlace.description,
+                        isValid: true
+                    }
                 },
-                description: {
-                    value: identifiedPlace.description,
-                    isValid: true
-                }
-            },
-            true
-        );
+                true
+            );
+        }
+
         setIsLoading(false);
     }, [setFormData, identifiedPlace]);
 
@@ -76,7 +80,9 @@ const UpdatePlace = () => {
     if (!identifiedPlace) {
         return (
             <div className="center">
-                <h2>Could not find the place</h2>
+                <Card>
+                    <h2>Could not find the place</h2>
+                </Card>
             </div>
         );
     }
