2061d167c622e0b96b16bbdd896aae0d8dacc6f8
Ozgun <ozgun.abanoz@gmail.com>
2019-12-05 11:47:49 +0300

    Configure backend for image upload

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const usersController = require('./../controllers/users-controller');
+const fileUpload = require('./../middleware/file-upload');
 
 const router = express.Router();
 
 router.get('/', usersController.getUsers);
 router.post(
     '/signup',
+    fileUpload.single('image'),
     [
         check('name')
             .not()
