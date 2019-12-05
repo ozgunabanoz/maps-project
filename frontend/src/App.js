ff39dd61888f7f84cbd89b9db1725bca5be5a9f9
Ozgun <ozgun.abanoz@gmail.com>
2019-12-05 16:28:56 +0300

    Auto login & auto logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Route,
@@ -14,22 +14,64 @@ import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
 import { AuthContext } from './shared/context/auth-context';
 
+let logOutTimer;
+
 const App = () => {
     const [token, setToken] = useState(false);
     const [userId, setUserId] = useState(false);
+    const [tokenExpirationDate, setTokenExpirationDate] = useState();
 
-    const login = useCallback((uid, token) => {
+    const login = useCallback((uid, token, expirationDate) => {
         setToken(token);
+        setUserId(uid);
+
+        const tokenExpDate =
+            expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+        setTokenExpirationDate(tokenExpDate);
+
         localStorage.setItem(
             'userData',
-            JSON.stringify({ userId: uid, token: token })
+            JSON.stringify({
+                userId: uid,
+                token: token,
+                expiration: tokenExpDate.toISOString()
+            })
         );
-        setUserId(uid);
     }, []);
+
     const logout = useCallback(() => {
         setToken(null);
         setUserId(null);
+        setTokenExpirationDate(null);
+        localStorage.removeItem('userData');
     }, []);
+
+    useEffect(() => {
+        if (token && tokenExpirationDate) {
+            const remTime =
+                tokenExpirationDate.getTime() - new Date().getTime();
+
+            logOutTimer = setTimeout(logout, remTime);
+        } else {
+            clearTimeout(logOutTimer);
+        }
+    }, [token, logout, tokenExpirationDate]);
+
+    useEffect(() => {
+        const storedData = JSON.parse(localStorage.getItem('userData'));
+
+        if (
+            storedData &&
+            storedData.token &&
+            new Date(storedData.expiration) > new Date()
+        ) {
+            login(
+                storedData.userId,
+                storedData.token,
+                new Date(storedData.expiration)
+            );
+        }
+    }, [login]);
 
     let routes;
 
